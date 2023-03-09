1276c8847af0617819627d9556182e6f2274e51a
Ramiz Agaev <webabduzeedo@gmail.com>
2023-03-09 19:03:05 +0500

    init project

---
+++ src/App.vue
@@ -0,0 +1,6 @@
+<script setup>
+</script>
+
+<template>
+  <div>asdaad</div>
+</template>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import './styles/main.scss'
+import App from './App.vue'
+
+
+createApp(App).mount('#app')
